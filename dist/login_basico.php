14f176cfa0cd2a785aef5126af68a25964b95431
Yan Leandro <yanleandro658@gmail.com>
2022-01-21 13:52:36 +0100

    nuevo

---
+++ dist/login_basico.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nombre introducido: ". $_POST['nombre']. "<br>";
+echo "Nombre introducido: ". $_POST['apellidos']. "<br>";
+echo "Nombre introducido: ". $_POST['username']. "<br>";
+echo "Nombre introducido: ". $_POST['ciudad']. "<br>";
+echo "Nombre introducido: ". $_POST['state']. "<br>";
+echo "Nombre introducido: ". $_POST['zip']. "<br>";
